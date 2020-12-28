1a8abb8f02ecea6d34406c247b674ce9790ecb95
chenggang_glb <1278063477@qq.com>
2020-12-28 16:53:17 +0800

    first version

---
+++ day01_git/src/main/java/com/atguigu/pojo/User.java
@@ -0,0 +1,11 @@
+package com.atguigu.pojo;
+
+/**
+ * @author chenggang
+ * @create 2020-12-28 16:44
+ */
+public class User {
+    public static void main(String[] args) {
+        System.out.println("qq");
+    }
+}
